a6e077ed623b6b861697eff291d6e11821ffcd37
santhoshkumar187 <santhoshkumars.20cse@kongu.edu>
2022-08-10 22:50:07 +0530

    freshbites1

---
+++ contactjs.js
@@ -0,0 +1,16 @@
+http = require('http');
+url = require('url');
+querystring = require('querystring');
+function onRequest(request,response)
+{
+    var path = url.parse(request.url).pathname;
+    console.log('Request for'+path+'received');
+    var query = url.parse(request.url).query;
+    console.log('query');
+    var name = querystring.parse(query)["username"];
+    var email = querystring.parse(query)["email"];
+    response.write('hello'+name+' your email id is'+email);
+    response.end();
+}
+http.createServer(onRequest).listen(8000);
+console.log('started....');
